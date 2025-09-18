f78555d3c2262b98feb4c83f095c55839563ca70
Muriel Salvan <muriel@x-aeon.com>
2025-09-18 18:37:13 +0200

    Fixed accuracy measure

--- lib/ruby_neural_nets/accuracy.rb
+++ lib/ruby_neural_nets/accuracy.rb
@@ -10,7 +10,7 @@ module RubyNeuralNets
     # Result::
     # * Float: Corresponding accuracy
     def measure(output_pred, output_real)
-      (output_real - output_pred.gt(0.5)).abs.sum(axis: 0).eq(0).count.to_f / output_real.shape[1]
+      (output_real - final_prediction(output_pred)).abs.sum(axis: 0).eq(0).count.to_f / output_real.shape[1]
     end
 
     # Get the confusion matrix between a predicted output and the real one
@@ -21,7 +21,21 @@ module RubyNeuralNets
     # Result::
     # * Numo::DFloat: Corresponding confusion matrix
     def confusion_matrix(output_pred, output_real)
-      output_real.dot(Numo::DFloat[output_pred.gt(0.5).transpose][0,nil,nil])
+      output_real.dot(Numo::DFloat[final_prediction(output_pred).transpose][0,nil,nil])
+    end
+
+    private
+
+    # Compute the final prediction from the model output
+    #
+    # Parameters::
+    # * *model_output* (Numo::DFloat): The model output
+    # Result::
+    # * Numo::DFloat: The final prediction
+    def final_prediction(model_output)
+      out = Numo::DFloat.zeros(*model_output.shape)
+      out[model_output.max_index(axis: 0)] = 1
+      out
     end
 
   end
