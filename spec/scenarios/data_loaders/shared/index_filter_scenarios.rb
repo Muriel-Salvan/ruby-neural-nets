4c988fb20e6909761c313799ecf3e74f64a678b8
Muriel Salvan <muriel@x-aeon.com>
2026-01-14 18:21:01 +0100

    Update index filter tests with compatible PNG values
    - Use PNG color values that survive Torch 8-bit conversion
    - Values like 257, 514, etc. normalize to same float for both Numo and Torch
    - Ensures tests work with different normalization pipelines

--- spec/scenarios/data_loaders/shared/index_filter_scenarios.rb
+++ spec/scenarios/data_loaders/shared/index_filter_scenarios.rb
@@ -4,71 +4,71 @@ RSpec.shared_examples 'index filter scenarios' do |options|
 
     it 'filters correctly with individual indexes' do
       with_test_dir(
-        'test_dataset/class_0/test_image_0.png' => png(1, 1, { color: [1] }),
-        'test_dataset/class_0/test_image_1.png' => png(1, 1, { color: [2] }),
-        'test_dataset/class_0/test_image_2.png' => png(1, 1, { color: [3] }),
-        'test_dataset/class_0/test_image_3.png' => png(1, 1, { color: [4] }),
-        'test_dataset/class_0/test_image_4.png' => png(1, 1, { color: [5] })
+        'test_dataset/class_0/test_image_0.png' => png(1, 1, { color: [257] }),
+        'test_dataset/class_0/test_image_1.png' => png(1, 1, { color: [514] }),
+        'test_dataset/class_0/test_image_2.png' => png(1, 1, { color: [771] }),
+        'test_dataset/class_0/test_image_3.png' => png(1, 1, { color: [1028] }),
+        'test_dataset/class_0/test_image_4.png' => png(1, 1, { color: [1285] })
       ) do |datasets_path|
         data_loader = new_data_loader(datasets_path:, filter_dataset: '1,3', partitions: { training: 1.0, dev: 0.0, test: 0.0 }, minmax_normalize: false)
         training_samples = data_loader.dataset(:training).first.to_a
         expect(training_samples.size).to eq(2)
-        # Check that we have samples with colors 2 and 4 (indexes 1 and 3)
+        # Check that we have samples with colors 514 and 1028 (indexes 1 and 3)
         colors = training_samples.map { |sample| options[:color_from].call(sample.input).to_f }.sort
-        expect(colors).to eq([2.0/65535, 4.0/65535])
+        expect(colors).to eq([514.0/65535, 1028.0/65535])
       end
     end
 
     it 'filters correctly with ranges' do
       with_test_dir(
-        'test_dataset/class_0/test_image_0.png' => png(1, 1, { color: [1] }),
-        'test_dataset/class_0/test_image_1.png' => png(1, 1, { color: [2] }),
-        'test_dataset/class_0/test_image_2.png' => png(1, 1, { color: [3] }),
-        'test_dataset/class_0/test_image_3.png' => png(1, 1, { color: [4] }),
-        'test_dataset/class_0/test_image_4.png' => png(1, 1, { color: [5] }),
-        'test_dataset/class_0/test_image_5.png' => png(1, 1, { color: [6] })
+        'test_dataset/class_0/test_image_0.png' => png(1, 1, { color: [257] }),
+        'test_dataset/class_0/test_image_1.png' => png(1, 1, { color: [514] }),
+        'test_dataset/class_0/test_image_2.png' => png(1, 1, { color: [771] }),
+        'test_dataset/class_0/test_image_3.png' => png(1, 1, { color: [1028] }),
+        'test_dataset/class_0/test_image_4.png' => png(1, 1, { color: [1285] }),
+        'test_dataset/class_0/test_image_5.png' => png(1, 1, { color: [1542] })
       ) do |datasets_path|
         data_loader = new_data_loader(datasets_path:, filter_dataset: '1-3,5', partitions: { training: 1.0, dev: 0.0, test: 0.0 }, minmax_normalize: false)
         training_samples = data_loader.dataset(:training).first.to_a
         expect(training_samples.size).to eq(4)
-        # Check that we have samples with colors 2,3,4,6 (indexes 1,2,3,5)
+        # Check that we have samples with colors 514,771,1028,1542 (indexes 1,2,3,5)
         colors = training_samples.map { |sample| options[:color_from].call(sample.input).to_f }.sort
-        expect(colors).to eq([2.0/65535, 3.0/65535, 4.0/65535, 6.0/65535])
+        expect(colors).to eq([514.0/65535, 771.0/65535, 1028.0/65535, 1542.0/65535])
       end
     end
 
     it 'includes all indexes when filter is "all"' do
       with_test_dir(
-        'test_dataset/class_0/test_image_0.png' => png(1, 1, { color: [1] }),
-        'test_dataset/class_0/test_image_1.png' => png(1, 1, { color: [2] }),
-        'test_dataset/class_0/test_image_2.png' => png(1, 1, { color: [3] })
+        'test_dataset/class_0/test_image_0.png' => png(1, 1, { color: [257] }),
+        'test_dataset/class_0/test_image_1.png' => png(1, 1, { color: [514] }),
+        'test_dataset/class_0/test_image_2.png' => png(1, 1, { color: [771] })
       ) do |datasets_path|
         data_loader = new_data_loader(datasets_path:, filter_dataset: 'all', partitions: { training: 1.0, dev: 0.0, test: 0.0 }, minmax_normalize: false)
         training_samples = data_loader.dataset(:training).first.to_a
         expect(training_samples.size).to eq(3)
         # Check that we have all samples
         colors = training_samples.map { |sample| options[:color_from].call(sample.input).to_f }.sort
-        expect(colors).to eq([1.0/65535, 2.0/65535, 3.0/65535])
+        expect(colors).to eq([257.0/65535, 514.0/65535, 771.0/65535])
       end
     end
 
     it 'partitions correctly with filtered indexes' do
       with_test_dir(
-        'test_dataset/class_0/test_image_0.png' => png(1, 1, { color: [1] }),
-        'test_dataset/class_0/test_image_1.png' => png(1, 1, { color: [2] }),
-        'test_dataset/class_0/test_image_2.png' => png(1, 1, { color: [3] }),
-        'test_dataset/class_0/test_image_3.png' => png(1, 1, { color: [4] }),
-        'test_dataset/class_0/test_image_4.png' => png(1, 1, { color: [5] })
+        'test_dataset/class_0/test_image_0.png' => png(1, 1, { color: [257] }),
+        'test_dataset/class_0/test_image_1.png' => png(1, 1, { color: [514] }),
+        'test_dataset/class_0/test_image_2.png' => png(1, 1, { color: [771] }),
+        'test_dataset/class_0/test_image_3.png' => png(1, 1, { color: [1028] }),
+        'test_dataset/class_0/test_image_4.png' => png(1, 1, { color: [1285] })
       ) do |datasets_path|
-        data_loader = new_data_loader(datasets_path:, filter_dataset: '0,2,4', partitions: { training: 0.6, dev: 0.4, test: 0.0 })
+        data_loader = new_data_loader(datasets_path:, filter_dataset: '0,2,4', partitions: { training: 0.6, dev: 0.4, test: 0.0 }, minmax_normalize: false)
         # Should have 3 elements total (filtered), distributed as training: 2, dev: 1
         training_samples = data_loader.dataset(:training).first.to_a
         dev_samples = data_loader.dataset(:dev).first.to_a
         expect(training_samples.size).to eq(2)
         expect(dev_samples.size).to eq(1)
-        # Check colors: original indexes 0,2,4 have colors 1,3,5
+        # Check colors: original indexes 0,2,4 have colors 257,771,1285
         all_colors = (training_samples + dev_samples).map { |sample| options[:color_from].call(sample.input).to_f }
-        expect(all_colors.sort).to eq([1.0/65535, 3.0/65535, 5.0/65535])
+        expect(all_colors.sort).to eq([257.0/65535, 771.0/65535, 1285.0/65535])
       end
     end
 
