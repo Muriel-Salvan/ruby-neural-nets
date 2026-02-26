ce70e651e98e4fb791cc504703cc40cb30d1879e
Muriel Salvan <muriel@x-aeon.com>
2026-02-26 11:42:30 +0100

    Add TEST_DEBUG=1 support for test debug logging (#29)
    When the TEST_DEBUG environment variable is set to 1, the test framework
    now enables debug mode and tees all log output to both the test StringIO
    buffer (for assertions) and STDOUT (for developer visibility).
    
    Changes:
    - Add TeeIO class in spec_helper.rb to write to multiple IO targets
    - Conditionally enable Logger.debug_mode and dual output in before(:each)
    - Update README with TEST_DEBUG documentation in Testing section
    
    Co-authored by: Cline (minimax/minimax-m2.5)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,35 @@ require 'byebug'
 
 require 'ruby_neural_nets_test/helpers'
 
+# IO adapter that writes to multiple targets simultaneously.
+# Used to tee log output to both StringIO (for test assertions) and STDOUT (for debugging).
+class TeeIO
+
+  # Create a new TeeIO that delegates writes to all given targets
+  #
+  # Parameters::
+  # * *targets* (Array<IO>): IO objects to write to
+  def initialize(*targets)
+    @targets = targets
+  end
+
+  # Write data to all targets
+  #
+  # Parameters::
+  # * *args* (Array): Arguments to pass to each target's write method
+  # Result::
+  # * Integer: Number of bytes written (from the first target)
+  def write(*args)
+    @targets.each { |t| t.write(*args) }
+  end
+
+  # Close all targets
+  def close
+    @targets.each(&:close)
+  end
+
+end
+
 RSpec.configure do |config|
   config.include RubyNeuralNetsTest::Helpers
   scenarios_dirs = ['scenarios']
@@ -16,11 +45,16 @@ RSpec.configure do |config|
   scenarios_dirs << platform_scenarios_dir if File.exist?("spec/#{platform_scenarios_dir}")
   config.pattern = scenarios_dirs.map { |dir| "spec/#{dir}/**/*_spec.rb" }.join(',')
 
-  # Set up test logger that captures output to a String instead of STDOUT
+  # Set up test logger that captures output to a String instead of STDOUT.
+  # When TEST_DEBUG=1, also enable debug mode and tee output to STDOUT.
   config.before(:each) do
-    # Create a StringIO to capture log output
     @test_log_stringio = StringIO.new
-    # Replace the global logger with our test logger
-    RubyNeuralNets::Logger.logger = ::Logger.new(@test_log_stringio)
+    if ENV['TEST_DEBUG'] == '1'
+      RubyNeuralNets::Logger.debug_mode = true
+      RubyNeuralNets::Logger.logger = ::Logger.new(TeeIO.new(@test_log_stringio, STDOUT))
+    else
+      RubyNeuralNets::Logger.debug_mode = false
+      RubyNeuralNets::Logger.logger = ::Logger.new(@test_log_stringio)
+    end
   end
 end
